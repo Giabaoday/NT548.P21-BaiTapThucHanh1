081dcb67f1d38c8c98ce193ea17f2c93f1d0690a
BinhMinh04 <22520871@gm.edu.vn>
2025-05-04 13:04:50 +0700

    new changes

--- modules/nat_gateway/outputs.tf
+++ modules/nat_gateway/outputs.tf
@@ -1,4 +1,5 @@
 output "nat_gateway_id" {
   description = "NAT Gateway ID"
-  value       = aws_nat_gateway.main.id
+  #value       = aws_nat_gateway.main.id
+  value       = aws_nat_gateway.nat.id
 }
