e0a142b61a2cf614a598b2838684b8fd6876c306
Trần Gia Bảo <142662904+Giabaoday@users.noreply.github.com>
2025-05-06 21:31:08 +0700

    Update allowed IP

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "public_sg" {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = ["172.16.0.98/32"]
+      cidr_blocks = ["118.70.53.89/23"]
     }
   ]
 
