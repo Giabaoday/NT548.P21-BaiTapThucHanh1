a1b3de1c7ed1e866cdd4401aa09c8094046239fa
BinhMinh04 <22520871@gm.edu.vn>
2025-05-06 10:12:28 +0700

    newchanges at route_tables/main.tf

--- modules/route_tables/main.tf
+++ modules/route_tables/main.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = var.internet_gateway_id
   }
 
@@ -15,7 +15,7 @@ resource "aws_route_table" "private" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = var.nat_gateway_id
   }
 
